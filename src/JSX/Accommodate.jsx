f3c0f20831afe226e392983d82a72e74fe23420d
hansojin <jjaren746@github.com>
2023-05-08 23:04:56 +0900

    'useState_counter'

---
+++ src/JSX/Accommodate.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import useCounter from "./useCounter";
+
+const MAX_CAPACITY = 10;
+
+function Accommodate(props) {
+    const [isFull, setIsFull] = useState(false);
+    const [count, increaseCount, decreaseCount] = useCounter(0);
+
+    useEffect(() => {
+        console.log("======================");
+        console.log("useEffect() is called.");
+        console.log(`isFull: ${isFull}`);
+    });
+
+    useEffect(() => {
+        setIsFull(count >= MAX_CAPACITY);
+        console.log(`Current count value: ${count}`);
+    }, [count]);
+
+    return (
+        <div style={{ padding: 16 }}>
+            <p>{`총 ${count}명 수용했습니다.`}</p>
+
+            <button onClick={increaseCount} disabled={isFull}>IN</button>
+            <button onClick={decreaseCount}>OUT</button>
+
+            {isFull && <p style={{ color: "red" }}>IT'S FULL</p>}
+        </div>
+    );
+}
+
+export default Accommodate;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import NotificationList from './JSX/NotificationList';
+import Accommodate from './JSX/Accommodate'; 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accommodate />
   </React.StrictMode>
 );
 
